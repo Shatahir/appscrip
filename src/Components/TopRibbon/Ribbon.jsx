5c5c7f01bc4982992612e7b4cad39024d35971b0
Shatahir <s.taher1996@gmail.com>
2024-03-03 23:56:33 +0530

    add some style

--- src/Components/TopRibbon/Ribbon.jsx
+++ src/Components/TopRibbon/Ribbon.jsx
@@ -7,19 +7,19 @@ function Ribbon() {
     <>
       <div className='ribbon flex  items-center justify-center bg-black text-white py-2'>
         <div className='flex flex-1 w-32 items-center justify-center'>
-          <img src='images\top-icon.png' alt='top-icon' className='pe-2' />
+          <img src='images/top-icon.png' alt='top-icon' className='pe-2' />
           <span style={{ color: '#EB4C6B', fontSize: 12 }}>
             lorem ipsom dolor
           </span>
         </div>
         <div className='flex flex-1 w-32 items-center justify-center max-sm:hidden'>
-          <img src='images\top-icon.png' alt='top-icon' className='pe-2' />
+          <img src='images/top-icon.png' alt='top-icon' className='pe-2' />
           <span style={{ color: '#EB4C6B', fontSize: 12 }}>
             lorem ipsom dolor
           </span>
         </div>
         <div className='flex flex-1 w-32 items-center justify-center max-sm:hidden'>
-          <img src='images\top-icon.png' alt='top-icon' className='pe-2' />
+          <img src='images/top-icon.png' alt='top-icon' className='pe-2' />
           <span style={{ color: '#EB4C6B', fontSize: 12 }}>
             lorem ipsom dolor
           </span>
